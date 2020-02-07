3d8881cad910137856d53154e2c7ea3402cedbc9
Michal Goliasz <mihgol.js@gmail.com>
2020-02-07 23:33:04 +0100

    added game setup objects

---
+++ src/Setup.ts
@@ -0,0 +1,22 @@
+namespace Tetris {
+    export const GAME_OPTIONS = {
+        rows: 20,
+        columns: 20,
+    }
+
+    export const DISPLAY_OPTIONS = {
+        blockSize: 25,
+        gapSize: 3,
+    }
+
+    export enum COLORS {
+        'black',
+        'crimson',
+        'darkcyan',
+        'darkslategrey',
+        'darkseagreen',
+        'yellowgreen',
+        'mediumslateblue',
+        'steelblue'
+    }
+}
