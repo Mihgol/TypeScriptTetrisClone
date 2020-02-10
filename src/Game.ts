c7f8a4b55a856686e3a42d53e73a94ef8dcf6fc5
Michal Goliasz <mihgo.js@gmail.com>
2020-02-10 16:59:46 +0100

    added game class with game-loop

---
+++ src/Game.ts
@@ -0,0 +1,22 @@
+namespace Tetris {
+    export class Game {
+        activeTetromino: Tetromino;
+        display: Display;
+        constructor() {
+            this.activeTetromino = new Tetromino();
+            this.display = new Display(document.body);
+            this.loop();
+        }
+
+        private loop() {
+
+            this.display.drawTetromino(this.activeTetromino);
+
+            setTimeout(() => {
+                requestAnimationFrame(()=>{
+                    this.loop();
+                })
+            }, 50)
+        }
+    }
+}
